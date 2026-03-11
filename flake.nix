b026235ceb0ad0419bce6c77b2fe2afde50f65d4
Svetlin Ralchev <iamralch@users.noreply.github.com>
2026-03-11 09:46:00 +0400

    build: migrate to nix flakes for development environment
    Replace Docker-based devcontainer with Nix flakes configuration. Simplify Dockerfile to base image and add Nix feature with flake support. Create flake.nix and flake.lock to manage Go toolchain, PostgreSQL, and AWS CLI dependencies. Update devcontainer postCreateCommand to use nix develop.

---
+++ flake.nix
@@ -0,0 +1,38 @@
+{
+  description = "aws-aurora development shell";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      nixpkgs,
+      flake-utils,
+      ...
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            go
+            gotools
+            gopls
+            postgresql
+            awscli2
+            git
+          ];
+
+          shellHook = ''
+            export GOPATH="$HOME/go"
+            export PATH="$GOPATH/bin:$PATH"
+          '';
+        };
+      }
+    );
+}
